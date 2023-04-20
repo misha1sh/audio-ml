f291a66b5db5811188bb7dcf06f7d4be5be45a73
misha1sh <misha_she@mail.ru>
2023-04-20 14:50:45 +0300

    Some version

--- js_lib/punctuator.js
+++ js_lib/punctuator.js
@@ -1,21 +1,31 @@
 'use strict';
 
 const assert = require('chai').assert;
+const fs = require('fs')
 
-const ort = require('onnxruntime-web');
-const session_promise = ort.InferenceSession.create('../model.v1.onnx');
+const file = fs.readFileSync('./model.onnx').buffer;
+
+const ort = function() {
+  if (typeof window === 'undefined') {
+    return require('onnxruntime-node');
+  }
+  return require('onnxruntime-web');
+}();
+
+
+const session_promise = ort.InferenceSession.create(file); //, 0, file.byteLength,  { executionProviders: ['cpu'] });
 
 module.exports.getPunctuation = async (text) => {
   assert.typeOf(text, 'string');
 
-  let x = new Float32Array(16 * 363);
+  let x = new Float32Array(32 * 489);
   for (let i = 0; i < x.length; i++) {
     x[i] = 1;
   }
-
-  const tensor = new ort.Tensor('float32', x, [1, 16, 363]);
+  console.log(x);
+  const tensor = new ort.Tensor('float32', x, [1 , 32, 489]);
   const session = await session_promise;
-  const output = (await session.run({'inputt': tensor})).output;
+  const output = (await session.run({input: tensor})).output;
   assert.deepStrictEqual(output.dims, [1, 3]);
 
 
